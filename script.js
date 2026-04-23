edffe3993130863e71def0452c0ab9e1c5da172b
Pedro Heinrich <pedroheinrich91@gmail.com>
2026-04-23 07:07:19 -0300

    Implementa função de exibir perfis do github
    -adiciona foto de perfil, nome e bio

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
 let div = document.querySelector('.perfil')
 
-document.querySelector('.busca').addEventListener('submit', async (event) => { // formulario de busca
-    event.preventDefault()
 
+
+function exibir(perfil) {
+
+    // Exibir avatar com bio e nome do perfil buscado
+    div.innerHTML = ''
+    let avatar = document.createElement('img') // criando elemento img com avatar
+    avatar.src = perfil.avatar_url
+
+    let nome = document.createElement('h1')// criando elemento h1 com nome
+    nome.innerText = perfil.name
+
+    let bio = document.createElement('h2')// criando elemento com bio 
+    bio.innerText = perfil.bio
+    
+    div.append(avatar,nome,bio) // adicionando na div 
+}
+
+
+async function buscaPerfil(event) {
+    event.preventDefault()
     let searchInput = document.querySelector('#searchInput').value // valor do campo de busca
     try {
-
         let response = await fetch(`https://api.github.com/users/${encodeURI(searchInput)}`, { // RESPOSTA da API 
             headers: {
                 'Authorization': `Bearer ${token}`
             }
         })
         let perfil = await response.json()// conversão em obj/json
-        console.log(perfil);     
+        exibir(perfil)// passando perfil por parâmetro
+        console.log(perfil);
         
+
 
         // pegando erro caso acontecer
     } catch (error) {
         console.log(`Erro cabuloso: ${error}`);
     }
-})
+}
+
+document.querySelector('.busca').addEventListener('submit', buscaPerfil)
+
+
+
