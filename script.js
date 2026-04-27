731144df7a46f174b1f367d3178e071f499531fd
Pedro Heinrich <pedroheinrich91@gmail.com>
2026-04-27 14:18:35 -0300

    Adiciona função de busca de Perfis do GitHub
    -Após colocar nome de usuario do github uma requisição é feita com a resposta da API
    
    -Usa a resposta da API para exibição do avatar, nome, usuario, bio e link para acessar o perfil.

--- script.js
+++ script.js
@@ -1,24 +1,65 @@
-let div = document.querySelector('.perfil')
+let busca_perfil = document.querySelector('.perfil')
+let img_Perfil = document.querySelector('#img_Perfil')
+let name_Perfil = document.querySelector('#Name_Perfil')
+let user_Perfil = document.querySelector('#User_Perfil')
+let bio_Perfil = document.querySelector('#Bio_Perfil')
+let valor_seguidores = document.querySelector('.valor_seguidores')
+let valor_seguindo = document.querySelector('.valor_seguindo')
+let link_perfil = document.querySelector('#link_perfil')
 
 
 
 function exibir(perfil) {
 
+    img_Perfil.innerHTML = ''
+    name_Perfil.innerHTML = ''
+    user_Perfil.innerHTML = ''
+    bio_Perfil.innerHTML = ''
+    valor_seguidores.innerHTML = ''
+    valor_seguindo.innerHTML = ''
+    link_perfil.innerHTML = ''
+
     // Exibir avatar com bio e nome do perfil buscado
-    div.innerHTML = ''
     let avatar = document.createElement('img') // criando elemento img com avatar
     avatar.src = perfil.avatar_url
 
-    let nome = document.createElement('h1')// criando elemento h1 com nome
+    let nome = document.createElement('h2')// criando elemento h1 com nome
     nome.innerText = perfil.name
 
-    let bio = document.createElement('h2')// criando elemento com bio 
+    let bio = document.createElement('h4')// criando elemento com bio 
     bio.innerText = perfil.bio
-    
-    div.append(avatar,nome,bio) // adicionando na div 
+
+    let user = document.createElement('h3')// criando elemento com bio 
+    user.innerText = `@${perfil.login}`
+
+    let seguidores = document.createElement('p')// criando elemento com bio 
+    seguidores.innerText = perfil.followers
+
+    let seguindo = document.createElement('p')// criando elemento com bio 
+    seguindo.innerText = perfil.following
+
+
+    // adicionando link de perfil pesquisado
+    link_perfil.href = perfil.html_url
+    link_perfil.textContent = perfil.html_url
+    link_perfil.target ='_blank'
+
+
+
+    img_Perfil.append(avatar) // adicionando foto de perfil
+    name_Perfil.append(nome) // adicionando nome do perfil
+    user_Perfil.append(user) // adicionando arroba@ do perfil
+    bio_Perfil.append(bio) // adicionando bio 
+    valor_seguidores.append(seguidores) // adicionando total de seguidores 
+    valor_seguindo.append(seguindo) // adicionando total de quantos o perfil está seguindo
+
+
+
+    // proximo passo é exibir valor total de repositórios, 
 }
 
 
+// fazendo a requisição da API
 async function buscaPerfil(event) {
     event.preventDefault()
     let searchInput = document.querySelector('#searchInput').value // valor do campo de busca
@@ -31,7 +72,7 @@ async function buscaPerfil(event) {
         let perfil = await response.json()// conversão em obj/json
         exibir(perfil)// passando perfil por parâmetro
         console.log(perfil);
-        
+
 
 
         // pegando erro caso acontecer
