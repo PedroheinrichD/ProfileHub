69ddf144130acfedbc3a94670f78f13c4b62d1ac
Pedro Heinrich <pedroheinrich91@gmail.com>
2026-04-28 13:23:35 -0300

    implementa função de perfil ativo quando se abre o site

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function exibir(perfil) {
     // adicionando link de perfil pesquisado
     link_perfil.href = perfil.html_url
     link_perfil.textContent = perfil.html_url
-    link_perfil.target ='_blank'
+    link_perfil.target = '_blank'
 
 
 
@@ -71,7 +71,6 @@ async function buscaPerfil(event) {
         })
         let perfil = await response.json()// conversão em obj/json
         exibir(perfil)// passando perfil por parâmetro
-        console.log(perfil);
 
 
 
@@ -85,3 +84,55 @@ document.querySelector('.busca').addEventListener('submit', buscaPerfil)
 
 
 
+// deixando um perfil ativo quando entrar no site 
+async function perfilAtivo() {
+    try {
+        let response = await fetch(`https://api.github.com/users/PedroHeinrichD`, { // RESPOSTA da API 
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        let perfil = await response.json()// conversão em obj/json
+        console.log(perfil);// vendo as propriedades 
+        
+
+
+        // Exibir avatar com bio e nome do perfil buscado
+        let avatar = document.createElement('img') // criando elemento img com avatar
+        avatar.src = perfil.avatar_url
+
+        let nome = document.createElement('h2')// criando elemento h1 com nome
+        nome.innerText = perfil.name
+
+        let bio = document.createElement('h4')// criando elemento com bio 
+        bio.innerText = perfil.bio
+
+        let user = document.createElement('h3')// criando elemento com bio 
+        user.innerText = `@${perfil.login}`
+
+        let seguidores = document.createElement('p')// criando elemento com bio 
+        seguidores.innerText = perfil.followers
+
+        let seguindo = document.createElement('p')// criando elemento com bio 
+        seguindo.innerText = perfil.following
+
+
+        // adicionando link de perfil pesquisado
+        link_perfil.href = perfil.html_url
+        link_perfil.textContent = perfil.html_url
+        link_perfil.target = '_blank'
+
+
+
+        img_Perfil.append(avatar) // adicionando foto de perfil
+        name_Perfil.append(nome) // adicionando nome do perfil
+        user_Perfil.append(user) // adicionando arroba@ do perfil
+        bio_Perfil.append(bio) // adicionando bio 
+        valor_seguidores.append(seguidores) // adicionando total de seguidores 
+        valor_seguindo.append(seguindo) // adicionando total de quantos o perfil está seguindo
+        // pegando erro caso acontecer
+    } catch (error) {
+        console.log(`Erro cabuloso: ${error}`);
+    }
+}
+perfilAtivo() 
